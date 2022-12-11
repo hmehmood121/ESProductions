1edb596dee4b4de386562e509937dab70d14b824
Hasan Mehmood <hmehmood121@gmail.com>
2022-12-11 09:54:47 +0500

    quick fix

--- pages/admin.js
+++ pages/admin.js
@@ -83,7 +83,7 @@ export default function Admin() {
 
           <div className="text-center lg:text-left">
             <button
-              type="submit"
+              onClick={logInWithEmailAndPassword}
               className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
             >
               Login
